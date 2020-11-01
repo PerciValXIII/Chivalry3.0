2faafc4615a7858d8b88f4a272ad89be0697fcb1
Swattik Maiti <50179165+PerciValXIII@users.noreply.github.com>
2020-11-01 19:00:15 +0530

    Add files via upload

---
+++ build/precache-manifest.f9c09321d1e4891741bd1a95f02ec891.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff43fde02a7999f9990d69b6e57e643a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcda20967139e0b6d0dd",
+    "url": "/static/css/main.7d0766d1.chunk.css"
+  },
+  {
+    "revision": "59573bcb8fddbc08e3bb",
+    "url": "/static/js/2.fed63959.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fed63959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcda20967139e0b6d0dd",
+    "url": "/static/js/main.a7f6d725.chunk.js"
+  },
+  {
+    "revision": "8f236be898f101036547",
+    "url": "/static/js/runtime-main.433a3f71.js"
+  },
+  {
+    "revision": "35c4dc60be08a56b3277f83c0f6b08f6",
+    "url": "/static/media/1.35c4dc60.jpg"
+  },
+  {
+    "revision": "32936431de1a7b4104123bb1184c95dd",
+    "url": "/static/media/2.32936431.jpg"
+  },
+  {
+    "revision": "51e6d51f75d80f19f5bce5eb03045d8a",
+    "url": "/static/media/3.51e6d51f.jpg"
+  },
+  {
+    "revision": "87992a33e38227493e43d954e58d0a54",
+    "url": "/static/media/4.87992a33.jpg"
+  },
+  {
+    "revision": "bfdeb040d69ec8df51c312f9c1132c09",
+    "url": "/static/media/5.bfdeb040.jpg"
+  },
+  {
+    "revision": "1ac5eb0ccee41788358108b1ae4c6292",
+    "url": "/static/media/alexandre-trouve-LP3HsuFxMxM-unsplash.1ac5eb0c.jpg"
+  },
+  {
+    "revision": "5374e24cafc94cdabb4ed8ff6f3e81c9",
+    "url": "/static/media/austin-wade-lNy-Yi0U-3o-unsplash.5374e24c.jpg"
+  },
+  {
+    "revision": "065349181b99e65df254c26e107d009e",
+    "url": "/static/media/ilya-gorborukov-syx5GyrkeZ8-unsplash.06534918.jpg"
+  },
+  {
+    "revision": "92c84e1de1513c18c5026987b2567539",
+    "url": "/static/media/image2.92c84e1d.png"
+  },
+  {
+    "revision": "ded1a9c6781f8a610cb3ba6902c1b398",
+    "url": "/static/media/image3.ded1a9c6.png"
+  },
+  {
+    "revision": "69d818602a3cd6cabef6c391fe91d758",
+    "url": "/static/media/nordwood-themes-Nv4QHkTVEaI-unsplash.69d81860.jpg"
+  },
+  {
+    "revision": "2792421c7ce9da9e343c6c122552deef",
+    "url": "/static/media/pedram-normohamadian-w-93ar9an2Q-unsplash.2792421c.jpg"
+  },
+  {
+    "revision": "0e86a5b7f81ef49952f426c26682d25a",
+    "url": "/static/media/sorin-sirbu-ifYt0sKdJYk-unsplash.0e86a5b7.jpg"
+  },
+  {
+    "revision": "7e36cd7ece6ca93ce4d9d41235b72360",
+    "url": "/static/media/svg-3.7e36cd7e.svg"
+  },
+  {
+    "revision": "61e69671f5a2beb23812ff6f5c99ea31",
+    "url": "/static/media/svg-4.61e69671.svg"
+  },
+  {
+    "revision": "1f7f8501643547a750b82343c70a0a22",
+    "url": "/static/media/svg-6.1f7f8501.svg"
+  },
+  {
+    "revision": "f7fef37fc94c5a601c158998c526fec8",
+    "url": "/static/media/video.f7fef37f.mp4"
+  }
+]);
